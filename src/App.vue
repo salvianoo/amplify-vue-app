86eb108969cdb74d30de82b28fa2a4d662beab9b
Salviano Ludgerio <salviano@fretebras.com.br>
2020-10-16 14:32:55 -0300

    qa: change welcome message

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js Amplify App"/>
+    <HelloWorld msg="Welcome to Your Vue.js Amplify App (qa1 environment)"/>
   </div>
 </template>
 
